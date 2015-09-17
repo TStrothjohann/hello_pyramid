31236cc21859ae99a620958c88d9b83af0133606
Hospitant Entwicklung <thomas@strothjohann.net>
2015-09-17 16:27:47 +0200

    added timeline_json view

--- hellopyramid/tests.py
+++ hellopyramid/tests.py
@@ -10,3 +10,8 @@ def test_my_view(pyramid_req):
     from views import my_view
     response = my_view(pyramid_req)
     assert response['greeting'] == 'Hello world!'
+
+def test_timeline_json(pyramid_req):
+    from views import timeline_json
+    response = timeline_json(pyramid_req)
+    assert response['title']['media']['caption'] == "Timeline"
